b9c44c194cbaf961b31079dc1b1a57715ece008d
IhsFace <65287148+IhsFace@users.noreply.github.com>
2023-09-21 09:58:00 +1000

    Create plugins.lua

---
+++ nvim/plugins.lua
@@ -0,0 +1,27 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },
+
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+      },
+    },
+  },
+}
+return plugins
