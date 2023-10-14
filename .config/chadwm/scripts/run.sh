f68ed5735a1d4bb6d35a4cbc1a016aca47bfe30c
IhsFace <ihsaanifzal75@gmail.com>
2023-10-14 12:43:43 +1100

    Added chadwm, st, eww, rofi

---
+++ .config/chadwm/scripts/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+dbus-update-activation-environment --all
+gnome-keyring-daemon --start --components=secrets
+source /etc/X11/xinit/xinitrc.d/50-systemd-user.sh
+xrdb merge ~/.Xresources 
+xbacklight -set 10 &
+feh --bg-fill ~/Pictures/wall/leaves-wall.png &
+xset r rate 200 50 &
+picom &
+nm-applet &
+blueman-applet &
+
+~/.config/chadwm/scripts/bar.sh &
+while type chadwm >/dev/null; do chadwm && continue || break; done
