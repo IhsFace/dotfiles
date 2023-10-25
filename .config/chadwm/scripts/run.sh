de13807957bf07f9d63924387573631460766ae9
IhsFace <ihsaanifzal75@gmail.com>
2023-10-25 21:30:35 +1100

    Added monitor

--- .config/chadwm/scripts/run.sh
+++ .config/chadwm/scripts/run.sh
@@ -3,6 +3,15 @@
 dbus-update-activation-environment --all
 gnome-keyring-daemon --start --components=secrets
 source /etc/X11/xinit/xinitrc.d/50-systemd-user.sh
+
+if xrandr | grep "HDMI-1-0 connected" > /dev/null; then
+	xrandr --output HDMI-1-0 --auto --output eDP-1 --off
+	sed -i 's/st.font:.*/st.font:	JetBrainsMono Nerd Font:style:medium:pixelsize=13/g' ~/.Xresources
+else
+	xrandr --output eDP-1 --auto --dpi 192
+	sed -i 's/st.font:.*/st.font:	JetBrainsMono Nerd Font:style:medium:pixelsize=26/g' ~/.Xresources
+fi
+
 xrdb merge ~/.Xresources 
 xbacklight -set 10 &
 feh --bg-fill ~/Pictures/wall/leaves-wall.png &
